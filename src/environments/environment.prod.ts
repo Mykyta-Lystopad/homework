eb19ce47747afd6f5d75ef6448f22e3998a3d933
Max Hontar <26366776+amaneshi@users.noreply.github.com>
2020-08-14 18:38:13 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import {Environment} from './interface';
 
 export const environment: Environment = {
-  api_url: 'https://homework.grassbusinesslabs.ml/',
+  api_url: 'https://homework.grassbusinesslabs.tk/',
   production: true,
   KEY_ROLE: 'user-role'
 };
