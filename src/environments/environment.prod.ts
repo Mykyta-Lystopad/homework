7103334c0c64174a12fed2ce2dd2be951ea262c8
nlistopad <nlistopad@colvir.ru>
2023-02-11 17:50:50 +0200

    remove comments lines

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 import {Environment} from './interface';
 
 export const environment: Environment = {
-  // api_url: 'https://homework.grassbusinesslabs.tk/',
-  api_url: 'https://laravelhomework.azurewebsites.net/',
+  api_url: 'https://laravelhomework.azurewebsites.net/public/',
   production: true,
   KEY_ROLE: 'user-role'
 };
