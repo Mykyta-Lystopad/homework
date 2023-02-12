dbf4be3a26a55cced61cbc307b89e22cb344570c
Mykyta-Lystopad <63310464+Mykyta-Lystopad@users.noreply.github.com>
2023-02-12 17:14:55 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
-import {Environment} from './interface';
+// import {Environment} from './interface';
 
-export const environment: Environment = {
-  api_url: 'https://laravelhomework.azurewebsites.net/',
-  production: true,
-  KEY_ROLE: 'user-role'
-};
+// export const environment: Environment = {
+//   api_url: 'https://laravelhomework.azurewebsites.net/',
+//   production: true,
+//   KEY_ROLE: 'user-role'
+// };
