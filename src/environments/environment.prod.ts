ce56afe7773a215b0e96a662a192aceaa7042e5b
nlistopad <nlistopad@colvir.ru>
2023-02-14 12:14:12 +0200

    change link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import {Environment} from './interface';
 
 export const environment: Environment = {
-  api_url: 'http://homework-back-master.dev.loc/public/',
+  api_url: 'http://homework-back-master.dev.loc/',
   production: true,
   KEY_ROLE: 'user-role'
 };
