edced7d9131e597d5fb3a0303cd9e67b2c55eadc
nlistopad <nlistopad@colvir.ru>
2023-02-21 21:14:06 +0200

    new link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import {Environment} from './interface';
 
 export const environment: Environment = {
-  api_url: 'http://homework-back-master.dev.loc/',
+  api_url: 'https://homeworkpproject.azurewebsites.net/public/',
   production: false,
   KEY_ROLE: 'user-role'
 };
