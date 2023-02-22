0db756a4e8878de227ed5adc0a06d58aa3e5bca8
nlistopad <nlistopad@colvir.ru>
2023-02-22 09:50:12 +0200

    chose a link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 import {Environment} from './interface';
 
 export const environment: Environment = {
-  api_url: 'https://homeworkpproject.azurewebsites.net/public/',
+  api_url: 'https://laravelhomework.azurewebsites.net/public/',
   production: true,
   KEY_ROLE: 'user-role'
 };
