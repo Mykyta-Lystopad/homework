b292074ccc9a0ed98a46e23206101b9d3455b383
Mykyta Lystopad <niktoring77@gmail.com>
2023-12-23 22:08:12 +0200

    jenkins_CI and CD implement sharedStages

--- sharedPipeline.groovy
+++ sharedPipeline.groovy
@@ -1,49 +1,36 @@
-def sharedStages() {
+// // sharedPipeline.groovy
+
+def sharedStages_test() {
     echo "Hello, World!"
 }
 
-return this
+def sharedStages() {
 
+    echo "stage = Fetch Docker Image Tags"
+    def registry = 'https://registry.hub.docker.com/v2/repositories/nominonik/google-class/tags'
 
-// // sharedPipeline.groovy
+    // Fetching tags from Docker Hub with authentication
+    def response = sh(script: "curl -u ${DOCKERHUB_USERNAME}:${DOCKERHUB_PASSWORD} -s ${registry}", returnStdout: true).trim()
 
-// def call() {
-//     // No need for 'return this'
-// }
+    echo "response: ${response}"
 
-// def sharedStages() {
-//     return {
-//         stage('Fetch Docker Image Tags') {
-//             steps {
-//                 script {
-//                     echo "stage = Fetch Docker Image Tags"
-//                     def registry = 'https://registry.hub.docker.com/v2/repositories/nominonik/google-class/tags'
+    // Parse the JSON response
+    def imageNames = sh(script: "echo ${response} | grep -oE 'google-class-[^,]*'", returnStdout: true).trim()
 
-//                     // Fetching tags from Docker Hub with authentication
-//                     def response = sh(script: "curl -u ${DOCKERHUB_USERNAME}:${DOCKERHUB_PASSWORD} -s ${registry}", returnStdout: true).trim()
+    echo "Docker Image Names: ${imageNames}"
 
-//                     echo "response: ${response}"
+    // Split the newline-separated string into a list of strings
+    def choicesList = imageNames.split('\n')
 
-//                     // Parse the JSON response
-//                     def imageNames = sh(script: "echo ${response} | grep -oE 'google-class-[^,]*'", returnStdout: true).trim()
+    // Convert Groovy list to Java list using collect
+    def javaChoicesList = choicesList.collect { it.trim() }
 
-//                     echo "Docker Image Names: ${imageNames}"
+    // Set the choices for the VERSION parameter
+    properties([
+        parameters([
+            string(name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Write the target environment (dev or qa)'),
+            choice(name: 'VERSION', choices: javaChoicesList, description: 'Choose the version')
+        ])
+    ])
 
-//                     // Split the newline-separated string into a list of strings
-//                     def choicesList = imageNames.split('\n')
-
-//                     // Convert Groovy list to Java list using collect
-//                     def javaChoicesList = choicesList.collect { it.trim() }
-
-//                     // Set the choices for the VERSION parameter
-//                     properties([
-//                         parameters([
-//                             string(name: 'ENVIRONMENT', defaultValue: 'dev', description: 'Write the target environment (dev or qa)'),
-//                             choice(name: 'VERSION', choices: javaChoicesList, description: 'Choose the version')
-//                         ])
-//                     ])
-//                 }
-//             }
-//         }
-//     }
-// }
+}
